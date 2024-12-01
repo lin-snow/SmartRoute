427dc12e076db8a49bf3370f99fe359b62a37616
L1nSn0w <l1nsn0w@qq.com>
2024-12-01 21:59:44 +0800

    Enhance HomeView.vue button styling and layout; improve admin link appearance with updated classes and hover effects for better user experience.

--- web/src/views/HomeView.vue
+++ web/src/views/HomeView.vue
@@ -1,10 +1,14 @@
 <template>
   <main class="w-screen h-auto">
-    <el-button class="shadow-xl absolute top-4 right-8" type="primary">
-      <span class="mr-1">
-        <el-icon><adminIcon /></el-icon>
+    <el-button
+      class="shadow-2xl absolute top-4 right-8 hover:scale-105 transition-transform duration-300 rounded-lg border-2 border-sky-400/50"
+      type="primary"
+      size="large"
+    >
+      <span class="mr-2 flex items-center">
+        <el-icon class="text-lg"><adminIcon /></el-icon>
       </span>
-        <router-link to="/login" class="mr-2">Admin</router-link>
+      <router-link to="/login" class="mr-1 font-mono font-bold text-base">Admin</router-link>
     </el-button>
     <HomeComp />
   </main>
